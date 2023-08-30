a4b70da8de08490f44d257a998cde355bd1bdf67
Pratik Valvi <pratikvalvi@pop-os.localdomain>
2023-08-30 18:32:56 +0530

    Footer section implemented

---
+++ components/Footer.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  AiOutlineGithub,
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+  AiOutlineYoutube,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+      <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
+      <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
+          <p>
+            Made with <span className="text-md text-red-700">❤️</span> by Pratik
+            Valvi © 2023<a href="/" className="hover:underline"></a>
+          </p>
+        </div>
+        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
+          <a
+            href="https://github.com/PratikValvi"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineGithub
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+          {/* <a
+            href="#"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineTwitter
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a> */}
+          <a
+            href="https://www.linkedin.com/in/pratik-rajendra-valvi/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineLinkedin
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCTXwPeE2v9Zsj6zhdhBjpwA"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineYoutube
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
